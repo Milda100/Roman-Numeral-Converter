94effc67061578760a9425b3c81842c1bed38a75
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-02-11 13:03:32 -0500

    Update Roman-Numeral-Converter.js

--- Roman-Numeral-Converter.js
+++ Roman-Numeral-Converter.js
@@ -4,8 +4,21 @@ const output = document.getElementById("output");
 
 
 const numberInput = () => {
+    const numberInt = parseInt(number.value);
+   
+    if (numberInt <= 0) {
+        output.innerText = `Please enter a number greater than or equal to 1`;
+        return;
+    } 
+
+    if (numberInt >= 4000) {
+        output.innerText = `Please enter a number less than or equal to 3999`;
+        return;
+    }
+
     if (!number.value) {
         output.innerText = `Please enter a valid number`;
+        return;
     }
 };
 
