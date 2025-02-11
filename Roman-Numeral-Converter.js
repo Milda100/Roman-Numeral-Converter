aed549490fbdbb30b4d783e973ac74a8d08bb881
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-02-11 16:13:01 -0500

    Update Roman-Numeral-Converter.js

--- Roman-Numeral-Converter.js
+++ Roman-Numeral-Converter.js
@@ -3,6 +3,38 @@ const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
 
+
+//contains a Roman numeral symbol and its corresponding value ("array pairs") (pair array of arrays)
+
+const pairArr = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+];
+
+const numberToRoman = (num) => {
+    let result = "";
+    const functionName = (array) => {
+    while (num >= array[1]) {
+        result += array[0];
+        num -= array[1];
+        }
+    };
+
+    pairArr.forEach(functionName);
+    return result;
+};
+
 const numberInput = () => {
     const numberInt = parseInt(number.value);
    
