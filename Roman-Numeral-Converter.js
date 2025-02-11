6d2f283a3441c17a601c3ba55c6f24c767f541cc
Jellyfish <157160973+Milda100@users.noreply.github.com>
2025-02-10 20:19:01 -0500

    Create Roman-Numeral-Converter.js

---
+++ Roman-Numeral-Converter.js
@@ -0,0 +1,18 @@
+const number = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+
+const numberInput = () => {
+    if (!number.value) {
+        output.innerText = `Please enter a valid number`;
+    }
+};
+
+convertBtn.addEventListener("click", numberInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        numberInput();
+    }
+  });
